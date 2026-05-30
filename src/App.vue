31d5ffdd9512009b26a74a40ffa7c371e20b5fcd
itschool <itschoolgithub@mail.ru>
2026-05-30 14:17:45 +0500

    vue-app

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <AppHeader />
+
+  <h1>{{ title }}</h1>
+  <button
+    @click="title = 'Hello World!'"
+  >Нажми на меня</button>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      title: 'Это тестовая страница'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <h1>Название сайта</h1>
+        <a
+        href="tel:+77777777777"
+        @click="call"
+        >+7 (777) 777 77 77</a>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        call(event) {
+            event.preventDefault();
+            alert('Вы позвонили!');
+        }
+    }
+}
+</script>
+
+<style>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: rgb(233, 233, 233);
+    }
+</style>
